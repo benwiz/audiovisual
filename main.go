e71ec421e0008c77676c1da2f04494e8ff76fba7
Ben Wisialowski <bwisialowski@gmail.com>
2018-06-27 21:47:59 -0500

    try to consume audio and video simultaneously

--- main.go
+++ main.go
@@ -1,54 +1,101 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"image"
+	"io"
 	"os"
-	"strconv"
 
+	"github.com/hajimehoshi/go-mp3"
+	"github.com/hajimehoshi/oto"
 	"gocv.io/x/gocv"
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("How to run:\n\tgo run main.go [camera ID]")
-		return
-	}
-
-	// parse args
-	deviceID, _ := strconv.Atoi(os.Args[1])
-
-	// open webcam
-	webcam, err := gocv.VideoCaptureDevice(deviceID)
+func runAudio(filename string) error {
+	f, err := os.Open(filename)
 	if err != nil {
-		fmt.Printf("error opening video capture device: %v\n", deviceID)
-		return
+		return err
 	}
-	defer webcam.Close()
+	defer f.Close()
 
-	// open display window
-	window := gocv.NewWindow("Face Blur")
+	d, err := mp3.NewDecoder(f)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+
+	p, err := oto.NewPlayer(d.SampleRate(), 2, 2, 8192)
+	if err != nil {
+		return err
+	}
+	defer p.Close()
+
+	fmt.Printf("Length: %d[bytes]\n", d.Length())
+
+	if _, err := io.Copy(p, d); err != nil {
+		return err
+	}
+	return nil
+}
+
+func runVideo(filename string) error {
+	video, err := gocv.VideoCaptureFile(filename)
+	if err != nil {
+		fmt.Printf("Error opening video capture file: %s\n", filename)
+		return err
+	}
+	defer video.Close()
+
+	window := gocv.NewWindow("Track Window")
 	defer window.Close()
 
-	// prepare image matrix
 	img := gocv.NewMat()
 	defer img.Close()
 
-	fmt.Printf("start reading camera device: %v\n", deviceID)
+	imgFG := gocv.NewMat()
+	defer imgFG.Close()
+
+	imgCleaned := gocv.NewMat()
+	defer imgCleaned.Close()
+
+	mog2 := gocv.NewBackgroundSubtractorMOG2()
+	defer mog2.Close()
+
 	for {
-		if ok := webcam.Read(&img); !ok {
-			fmt.Printf("cannot read device %d\n", deviceID)
-			return
+		if ok := video.Read(&img); !ok {
+			fmt.Printf("Error cannot read file %s\n", filename)
+			return errors.New("cannot read video file")
 		}
 		if img.Empty() {
 			continue
 		}
 
-		fmt.Println(img.GetDoubleAt(100, 100))
+		// clean frame by removing background & eroding to eliminate artifacts
+		mog2.Apply(img, &imgFG)
+		kernel := gocv.GetStructuringElement(gocv.MorphRect, image.Pt(3, 3))
+		gocv.Erode(imgFG, &imgCleaned, kernel)
+		kernel.Close()
 
-		// show the image in the window, and wait 1 millisecond
+		// show frame
 		window.IMShow(img)
 		if window.WaitKey(1) >= 0 {
 			break
 		}
 	}
+
+	return nil
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("How to run:\n\tmain [videoFile] [audioFile]")
+		return
+	}
+	fmt.Println(os.Args[0])
+	videoFilename := os.Args[1]
+	audioFilename := os.Args[2]
+
+	go runAudio(audioFilename)
+	runVideo(videoFilename)
 }
