ca13d3d0a1f5ff8a329e2b1cce32f2739efb4973
Ben Wiz <bwisialowski@gmail.com>
2018-12-15 08:26:08 +0700

    start finding a z value

--- src/net.cpp
+++ src/net.cpp
@@ -10,6 +10,9 @@
 Net::Net() {}
 
 void Net::setup(int w, int h) {
+  // TODO: Consider other shape options, it may make more sense to have a
+  // circle or even a sphere (or any shape?)
+
   int multiplier = 20;
 
   // Set primitive to triangles // OF_PRIMITIVE_TRIANGLES
@@ -24,6 +27,7 @@ void Net::setup(int w, int h) {
     }
   }
 
+  // Connect vertices with indices
   for (int y = 0; y < h - 1; y++) {
     for (int x = 0; x < w - 1; x++) {
       mesh.addIndex(x + y * w);       // 0
@@ -35,22 +39,42 @@ void Net::setup(int w, int h) {
       mesh.addIndex(x + (y + 1) * w);       // 10
     }
   }
+
+  // Save the center location of the mesh for later. Just X and Y, not Z.
+  center = ofVec2f(w * multiplier / 2, h * multiplier / 2);
 }
 
 void Net::update(Packet packet) {
   for (auto &vertex : mesh.getVertices()) {
 
-    // TODO: zOffset should calculate the distance from center of mesh then use
-    // that distance as a fraction of radius (max distance from center) to
-    // select a FrequencyBand or a MelBand to warp with.
+    // TODO: zOffset should calculate the X-Y (not X-Y-Z) distance from center
+    // of mesh then use that distance as a fraction of radius (max distance
+    // from center) to select a FrequencyBand or a MelBand to warp with.
 
-    float zOffset = 10 * (0.5 - ofRandomuf());
-    ofPoint offset(0, 0, zOffset);
-    vertex += offset;
+    // Calculate X-Y distance and maxDist to find distRatio
+    float dist = center.distance(ofVec2f(vertex.x, vertex.y));
+    float maxDist = sqrt(pow(center.x, 2) + pow(center.y, 2));
+    float distRatio = dist / maxDist;
+
+    // Select a melBand
+    int melBandIndex = distRatio * packet.melBands.size();
+    cout << melBandIndex << ":\t" << packet.melBands[melBandIndex] << endl;
+
+    // Calculate z value
+    float z = 10 * abs(packet.melBands[melBandIndex]) - 6;
+
+    // Update the vertex
+    //  float zOffset = melBandValue;
+    //  ofPoint offset(0, 0, zOffset);
+    //  vertex += offset;
+    vertex = ofVec3f(vertex.x, vertex.y, z);
   }
 }
 
 void Net::draw() {
   ofTranslate(-45, -45);
   mesh.drawWireframe();
+
+  //  ofSetColor(ofColor::green);
+  //  ofDrawSphere(center.x, center.y, 0, 5);
 }
--- src/net.h
+++ src/net.h
@@ -19,4 +19,5 @@ public:
 
 private:
   ofMesh mesh;
+  ofVec2f center;
 };
