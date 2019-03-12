5ca21fb4f9e8f8278b7f0a52d1d4b11c9c9b471b
Ben Wiz <bwisialowski@gmail.com>
2019-03-11 19:06:35 -0500

    interface AudioFeatures

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,26 @@
-console.log('hey');
+interface AudioFeatures {
+  danceability: number;
+  energy: number;
+  key: number;
+  loudness: number;
+  mode: number;
+  speechiness: number;
+  acousticness: number;
+  instrumentalness: number;
+  liveness: number;
+  valence: number;
+  tempo: number;
+  type: string;
+  id: string;
+  uri: string;
+  trackHref: string;
+  analysisUrl: string;
+  durationMs: number;
+  timeSignature: number;
+}
+
+const main = (): void => {
+  console.log('hey!');
+};
+
+main();
