cd7b18d674aada38c3d531b9d8aa95cbbbbff029
Ben Wiz <bwisialowski@gmail.com>
2019-04-28 14:04:34 -0600

    draw polyline tangents that look really cool

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -100,11 +100,54 @@ void Canvas::drawMelBands() {
 }
 
 void Canvas::drawRibbon() {
+    // Draw main polyline
     ofBackground(255);
     ofSetColor(0);  // Black for saved polylines
     for (int i = 0; i < polylines.size(); i++) {
         ofPolyline polyline = polylines[i];
         polyline.draw();
+        
+//        // Draw point at each vertex
+//        vector<glm::tvec3<float, glm::packed_highp>> vertices = polyline.getVertices();
+//        for (int vertexIndex = 0; vertexIndex < vertices.size(); vertexIndex++) {
+//            ofVec3f vertex = vertices[vertexIndex];
+//            ofDrawCircle(vertex, 5);
+//        }
+        
+//        // Draw point at every % along the line
+//        for (int p=0; p<=100; p+=10) {
+//            ofVec3f point = polyline.getPointAtPercent(p / 100.0);
+//            ofDrawCircle(point, 5);
+//        }
+        
+//        // Draw normals
+//        vector<glm::tvec3<float, glm::packed_highp>> vertices = polyline.getVertices();
+//        float normalLength = 50;
+//        for (int vertexIndex = 0; vertexIndex < vertices.size(); vertexIndex++) {
+//            ofVec3f vertex = vertices[vertexIndex];  // Get the vertex
+//            ofVec3f normal = polyline.getNormalAtIndex(vertexIndex) * normalLength;  // Scale the normal
+//            ofDrawLine(vertex - normal / 2, vertex + normal / 2);  // Center the scaled normal around the vertex
+//        }
+        
+//        // Draw tagents
+//        vector<glm::tvec3<float, glm::packed_highp>> vertices = polyline.getVertices();
+//        float tangentLength = 80;
+//        for (int vertexIndex=0; vertexIndex<vertices.size(); vertexIndex++) {
+//            ofVec3f vertex = vertices[vertexIndex];
+//            ofVec3f tangent = polyline.getTangentAtIndex(vertexIndex) * tangentLength;
+//            ofDrawLine(vertex-tangent/2, vertex+tangent/2);
+//        }
+        
+        // Draw tagents evenly
+        ofSetColor(0, 50);
+        float tangentLength = 300;
+        for (int p = 0; p < 500; p+=1) {
+            ofVec3f point = polyline.getPointAtPercent(p / 500.0);
+            float floatIndex = polyline.getIndexAtPercent(p / 500.0);
+            ofVec3f tangent = polyline.getTangentAtIndexInterpolated(floatIndex) * tangentLength;
+            ofDrawLine(point-tangent/2, point+tangent/2);
+        }
+        
     }
     ofSetColor(255,100,0);  // Orange color for active polyline
     currentPolyline.draw();
@@ -137,6 +180,7 @@ void Canvas::mouseReleased(int x, int y, int button) {
     if (button == OF_MOUSE_BUTTON_LEFT) {
         leftMouseButtonPressed = false;
         currentPolyline.curveTo(x, y); // Necessary duplicate for last control point
+        currentPolyline.simplify(0.75); // Simplify (remove duplicate points with a sensitivity of 0.75)
         polylines.push_back(currentPolyline);
         currentPolyline.clear();  // Erase the vertices, allows us to start a new brush stroke
     }
